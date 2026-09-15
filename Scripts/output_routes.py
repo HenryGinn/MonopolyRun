5824e8f18be1f88bfefd3432dd4ed6c0ddd67209
Henry Ginn <henryginn137@gmail.com>
2026-09-15 06:53:43 +0100

    Remove poorly places labels in appendix

--- Scripts/output_routes.py
+++ Scripts/output_routes.py
@@ -14,6 +14,9 @@ from monopoly import Monopoly
 
 class PlottedRoute():
 
+    label_distance_from_point = 0.025
+    labels_too_close_threshold = 0.04
+
     def __init__(self, name):
         self.monopoly = Monopoly(name)
         self.monopoly.setup()
@@ -60,7 +63,7 @@ class PlottedRoute():
         angles = np.linspace(0, 2*np.pi, 19, endpoint=False)
         self.directions = np.stack(
             (np.sin(angles), np.cos(angles))
-            ).T * 0.12
+            ).T * self.label_distance_from_point
 
 
     def generate_routes(self):
@@ -80,6 +83,7 @@ class PlottedRoute():
         self.set_vertices()
         self.set_line_points()
         self.set_label_positions()
+        self.filter_label_positions()
         self.save()
 
     def set_speed_paths(self, speed):
@@ -99,13 +103,13 @@ class PlottedRoute():
     def normalise_coordinates(self):
         self.shift = self.coordinates.mean(axis=0)
         self.coordinates -= self.shift
-        self.scale = self.coordinates.std()
+        self.scale = np.max(self.coordinates.max(axis=0) - self.coordinates.min(axis=0))
         self.coordinates /= self.scale
 
     def set_vertices(self):
         self.vertices = self.monopoly.solver.vertices_solution[["X", "Y", "Place"]]
         self.vertices.drop_duplicates(subset=["X", "Y"], inplace=True)
-        self.vertices["Place"] = self.vertices["Place"].map(self.id_lookup)
+        self.vertices["Label"] = self.vertices["Place"].map(self.id_lookup)
         self.vertices[["X", "Y"]] = (self.vertices[["X", "Y"]] - self.shift) / self.scale
 
     def set_line_points(self):
@@ -127,6 +131,71 @@ class PlottedRoute():
         label_position = possibilities[index]
         return label_position
 
+
+    # Sometimes labels are too close to each other or overlap. We solve
+    # this problem by removing some labels.
+    
+    def filter_label_positions(self):
+        self.set_terminal_lable_index()
+        self.remove_labels_nearer_to_other_points()
+        self.remove_overlapping_labels()
+
+    def set_terminal_lable_index(self):
+        self.terminal_lable_index = self.monopoly.places.loc[
+            self.monopoly.places["Square"] == self.monopoly.terminal].index[0]
+
+    def remove_labels_nearer_to_other_points(self):
+        differences = (
+            self.vertices[["LabelX", "LabelY"]].values[:, None, :]
+            - self.vertices[["X", "Y"]].values[None, :, :])
+        distances = np.linalg.norm(differences, axis=2)
+        np.fill_diagonal(distances, np.inf)
+        min_distances = distances.min(axis=1)
+        self.label_indexes = min_distances <= route.label_distance_from_point * 0.8
+        self.vertices.loc[self.vertices.index[self.label_indexes], "Label"] = ""
+
+    def remove_overlapping_labels(self):
+        self.labels_too_close = True
+        distances = self.get_distances_between_labels()
+        problematic_labels = self.get_problematic_labels(distances)
+        self.remove_problematic_labels(problematic_labels)
+
+    def get_distances_between_labels(self):
+        positions = route.vertices[["LabelX", "LabelY"]].values
+        differences = positions[:, None, :] - positions[None, :, :]
+        distances = np.linalg.norm(differences, axis=2)
+        np.fill_diagonal(distances, np.inf)
+        # Remove labels that were already removed in the previous step.
+        for index in self.label_indexes:
+            distances[index, :] = np.inf
+            distances[:, index] = np.inf
+        return distances
+
+    def get_problematic_labels(self, distances):
+        problematic_labels = []
+        while self.labels_too_close:
+            i, j = np.unravel_index(np.argmin(distances), distances.shape)
+            if distances[i, j] < self.labels_too_close_threshold:
+                problematic_labels.append((i, j))
+                distances[i, j] = np.inf
+                distances[j, i] = np.inf
+            else:
+                self.labels_too_close = False
+        return problematic_labels
+
+    def remove_problematic_labels(self, problematic_labels):
+        # There is a more sophisticated way of doing this. For example,
+        # if labels A, B, and C are in a line and are too close then the
+        # problem may be solved by removing just the B in the middle.
+        for label_index_pair in problematic_labels:
+            # Do not want to remove the label for the terminal
+            if label_index_pair[0] == self.terminal_lable_index:
+                label_index = label_index_pair[1]
+            else:
+                label_index = label_index_pair[0]
+            place_index = self.vertices.index[label_index]
+            self.vertices.loc[place_index, "Label"] = ""
+
     def save(self):
         np.savetxt(self.route_path, self.coordinates, fmt='%f')
         self.vertices.to_csv(self.place_path, index=False)
@@ -135,3 +204,6 @@ class PlottedRoute():
 def main(name):
     route = PlottedRoute(name)
     route.generate_routes()
+
+route = PlottedRoute("2026")
+route.generate_routes()
